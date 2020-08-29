3a05364680dcf118cb85d6dc4777cd3a44c7b363
arturlauth-dev <arturlauthmain@gmail.com>
2020-08-29 18:40:41 -0300

    criado formulário com os fields necessários
    close #6

--- cadastro/base/urls.py
+++ cadastro/base/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from cadastro.base import views
 
 urlpatterns = [
-    path('cadastrar', views.index, name='index')
+    path('cadastrar', views.cadastro_pessoa, name='cadastro_pessoa')
 ]
--- cadastro/base/views.py
+++ cadastro/base/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from cadastro.base.forms import CadastroForm
 
 
-def index(request):
-    return HttpResponse("view inicial")
+def cadastro_pessoa(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = CadastroForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+
+            form.save()
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return home(request)
+
+        else:
+            print(form.errors)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = CadastroForm()
+
+    return render(request, 'base/cadastro.html', {'form': form})
