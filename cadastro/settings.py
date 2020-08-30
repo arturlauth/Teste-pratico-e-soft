f08bb4f15d0ffad6acb329c0cf96716448d12ed8
arturlauth-dev <arturlauthmain@gmail.com>
2020-08-30 17:10:44 -0300

    front end
    close #14

--- cadastro/settings.py
+++ cadastro/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'bootstrap4',
     'bootstrap_datepicker_plus',
-    'crispy_forms',
     'ordered_model'
 ]
 
